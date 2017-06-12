380c0c03b322691ef2b3f614e9a678d8a2ac3e31
Halbout Michael <michaelhalbout@gmail.com>
2017-06-12 08:35:20 +0200

    Create panel.php

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -1,25 +1,25 @@
 <?php
-require_once('PhpSIP.class.php');
-
-/* Sends NOTIFY to reset Linksys phone */
-
-try
-{
-  $api = new PhpSIP();
-  $api->setUsername('10000'); // authentication username
-  $api->setPassword('secret'); // authentication password
-  // $api->setProxy('some_ip_here'); 
-  $api->addHeader('Event: resync');
-  $api->setMethod('NOTIFY');
-  $api->setFrom('sip:10000@sip.domain.com');
-  $api->setUri('sip:10000@sip.domain.com');
-  $res = $api->send();
-
-  echo "response: $res\n";
-  
-} catch (Exception $e) {
-  
-  echo $e;
-}
-
+	if (!isConnect()) {
+		throw new Exception('{{401 - Accès non autorisé}}');
+	}
+	$eqLogics = eqLogic::byType('clientSIP');
 ?>
+<div style="position : fixed;height:100%;width:15px;top:50px;left:0px;z-index:998;background-color:#f6f6f6;" id="bt_displayObjectList">
+	<i class="fa fa-arrow-circle-o-right" style="color : #b6b6b6;"></i>
+</div>
+<div class="row row-overflow" id="div_clientSIP">
+	<div class="col-xs-2" id="sd_objectList" style="z-index:999">
+		<div class="bs-sidebar">
+			<ul id="ul_object" class="nav nav-list bs-sidenav">
+				<li class="nav-header">{{Mes Clients SIP}}</li>
+				<?php
+					foreach ($eqLogics as $eqLogic) {
+						echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName() . '</a></li>';
+					}
+				?>
+			</ul>
+		</div>
+	</div>
+	<div class="col-xs-10" id="div_graphiqueDisplay">Affichage de tout les client sip	</div>
+</div>
+<?php include_file('desktop', 'panel', 'js', 'clientSIP');?>
