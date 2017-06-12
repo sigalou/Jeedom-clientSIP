8d47171bc0f02509723a85cf7299d5b65d2bc34c
Halbout Michael <michaelhalbout@gmail.com>
2017-06-12 13:47:32 +0200

    Create clientSIP.class.php

--- core/class/clientSIP.class.php
+++ core/class/clientSIP.class.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+include_file('core', 'PhpSIP', 'class', 'clientSIP');
 class clientSIP extends eqLogic {
 	public function preInsert() {
 	}
@@ -7,6 +8,24 @@ class clientSIP extends eqLogic {
 	}
 	public function postSave() {		
 	}	
+	private function ConnectSip(){
+		try{
+			$api = new PhpSIP();
+			$api->setUsername($this->getConfiguration("Username")); // authentication username
+			$api->setPassword($this->getConfiguration("Password")); // authentication password
+			// $api->setProxy('some_ip_here'); 
+			$api->addHeader('Event: resync');
+			$api->setMethod('NOTIFY');
+			$api->setFrom('sip:10000@sip.domain.com');
+			$api->setUri('sip:10000@sip.domain.com');
+			$res = $api->send();
+			
+			log::add('clientSIP','debug',"response: $res");
+
+		} catch (Exception $e) {
+			log::add('clientSIP','error',$e);
+		}
+	}
 }
 class clientSIPCmd extends cmd {
 	public function execute($_options = null){
