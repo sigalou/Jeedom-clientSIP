e0851b9f194d9c444996e5588e031d937cac2480
Halbout Michael <michaelhalbout@gmail.com>
2017-06-15 08:03:38 +0200

    Create clientSIP.class.php

--- core/class/clientSIP.class.php
+++ core/class/clientSIP.class.php
@@ -2,11 +2,38 @@
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 include_file('core', 'PhpSIP', 'class', 'clientSIP');
 class clientSIP extends eqLogic {
-	public function preInsert() {
+	public static function deamon_info() {
+		$return = array();
+		$return['log'] = 'clientSIP';
+		$return['launchable'] = 'ok';
+		$return['state'] = 'nok';
+		foreach(eqLogic::byType('clientSIP') as $clientSIP){
+			if($clientSIP->getIsEnable()){
+				$cron = cron::byClassAndFunction('clientSIP', 'ConnectSip', array('id' => $clientSIP->getId()));
+				if (!is_object($cron)) 	
+					return $return;
+			}
+		}
+		$return['state'] = 'ok';
+		return $return;
 	}
-	public function preSave() {
+	public static function deamon_start($_debug = false) {
+		log::remove('Volets');
+		self::deamon_stop();
+		$deamon_info = self::deamon_info();
+		if ($deamon_info['launchable'] != 'ok') 
+			return;
+		if ($deamon_info['state'] == 'ok') 
+			return;
+		foreach(eqLogic::byType('clientSIP') as $clientSIP)
+			$clientSIP->CreateDemon();
 	}
-	public function postSave() {		
+	public static function deamon_stop() {	
+		foreach(eqLogic::byType('clientSIP') as $clientSIP)
+			$cron = cron::byClassAndFunction('clientSIP', 'ConnectSip', array('id' => $clientSIP->getId()));
+			if (is_object($cron)) 	
+				$cron->remove();
+		}
 	}	
 	public function toHtml($_version = 'mobile',$Dialog=true) {
 		$User["User"]=$this->getConfiguration("Username");
@@ -26,6 +53,20 @@ class clientSIP extends eqLogic {
 		);	
 		return template_replace($replace, getTemplate('core', $_version, 'eqLogic','clientSIP'));
 	}
+	public function CreateDemon() {
+		$cron =cron::byClassAndFunction('clientSIP', 'ConnectSip', array('clientSIP' => $this->getId()));
+			if (!is_object($cron)) {
+				$cron = new cron();
+				$cron->setClass('clientSIP');
+				$cron->setFunction('ConnectSip');
+				$cron->setOption(array('id' => $this->getId()));
+				$cron->setEnable(1);
+				$cron->setDeamon(1);
+				$cron->save();
+			}
+				$cron->save();
+		return $cron;
+	}
 	private function ConnectSip(){
 		try{
 			$Host=config::byKey('Host', 'clientSIP');
@@ -36,13 +77,13 @@ class clientSIP extends eqLogic {
 			$api->setUsername($Username); // authentication username
 			$api->setPassword(); // authentication password
 			// $api->setProxy('some_ip_here'); 
-			$api->addHeader('Event: resync');
-			$api->setMethod('NOTIFY');
-			$api->setFrom('sip:'.$Username.'@'.$Host.':'.$Port);
-			$api->setUri('sip:'.$Username.'@'.$Host.':'.$Port);
-			$res = $api->send();
+			//$api->addHeader('Event: resync');
+			//$api->setMethod('NOTIFY');
+			//$api->setFrom('sip:'.$Username.'@'.$Host.':'.$Port);
+			//$api->setUri('sip:'.$Username.'@'.$Host.':'.$Port);
+			//$res = $api->send();
 			
-			log::add('clientSIP','debug',"response: $res");
+			//log::add('clientSIP','debug',"response: $res");
 
 		} catch (Exception $e) {
 			log::add('clientSIP','error',$e);
