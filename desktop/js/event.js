ec7c573af6138d3b9190f918c83cc38d68b05004
Halbout Michael <michaelhalbout@gmail.com>
2017-06-27 13:52:24 +0200

    Update event.js

--- desktop/js/event.js
+++ desktop/js/event.js
@@ -1,6 +1,39 @@
  $('body').on('clientSIP::call', function (_event,_options) {
-			$('#md_modal').dialog({
-        title: 'Appel en cours',
-        message:_options,
-        'open'});
+	bootbox.dialog({
+		title: "{{Ajout d'une nouvelle condition}}",
+		message: 'Un appel est en cours, voulez vous répondre',
+		buttons: {
+			"Racrocher": {
+				className: "btn-danger",
+				callback: function () {
+					updateCache('Racrocher');
+				}
+			},
+			success: {
+				label: "Décrocher",
+				className: "btn-success",
+				callback: function () {
+					updateCache('Decrocher');
+				}
+			},
+		}
+	});
 });
+function updateCache(reponse){
+	$.ajax({
+		type: 'POST',            
+		async: false,
+		url: 'plugins/clientSIP/core/ajax/clientSIP.ajax.php',
+		data:{
+			action: 'updateCache',
+			value:reponse
+		},
+		dataType: 'json',
+		global: false,
+		error: function(request, status, error) {},
+		success: function(data) {
+			if (!data.result)
+				$('#div_alert').showAlert({message: 'Aucun message reçu', level: 'error'});
+		}
+	});
+}
