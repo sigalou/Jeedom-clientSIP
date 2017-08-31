c7acdd570d20dbc0ec6f16a6edbb81004a46ec16
Halbout Michael <michaelhalbout@gmail.com>
2017-08-31 10:59:39 +0200

    Update panel.js

--- desktop/js/panel.js
+++ desktop/js/panel.js
@@ -1,34 +1,80 @@
-if((!isset(userProfils.doNotAutoHideMenu) || userProfils.doNotAutoHideMenu != 1) && !jQuery.support.touch){
-	$('#sd_objectList').hide();
-	$('#div_graphiqueDisplay').removeClass('col-xs-10').addClass('col-xs-12');
-	$('#bt_displayObjectList').on('mouseenter',function(){
-		var timer = setTimeout(function(){
-			$('#bt_displayObjectList').find('i').hide();
-			$('#div_graphiqueDisplay').addClass('col-xs-10').removeClass('col-xs-12');
-			$('#sd_objectList').show();
-			$(window).resize();
-		}, 100);
-		$(this).data('timerMouseleave', timer)
-	}).on("mouseleave", function(){
-		clearTimeout($(this).data('timerMouseleave'));
-	});
-	$('#sd_objectList').on('mouseleave',function(){
-		var timer = setTimeout(function(){
-			$('#sd_objectList').hide();
-			$('#bt_displayObjectList').find('i').show();
-			$('#div_graphiqueDisplay').removeClass('col-xs-10').addClass('col-xs-12');
-			setTimeout(function(){
-				$(window).resize();
-			},100);
-			setTimeout(function(){
-				$(window).resize();
-			},300);
-			setTimeout(function(){
-				$(window).resize();
-			},500);
-		}, 300);
-		$(this).data('timerMouseleave', timer);
-	}).on("mouseenter", function(){
-		clearTimeout($(this).data('timerMouseleave'));
-	});
+ setTimeout(function () {
+  positionEqLogic();
+  $('.div_displayEquipement').disableSelection();
+  $( "input").click(function() { $(this).focus(); });
+  $( "textarea").click(function() { $(this).focus(); });
+  $('.div_displayEquipement').each(function(){
+    var container = $(this).packery({
+      itemSelector: ".eqLogic-widget",
+      columnWidth:40,
+      rowHeight: 80,
+      gutter : 2,
+    });
+  });
+
+  $('#bt_editDashboardWidgetOrder').on('click',function(){
+    if($(this).attr('data-mode') == 1){
+      $.hideAlert();
+      $(this).attr('data-mode',0);
+      editWidgetMode(0);
+      $(this).css('color','black');
+    }else{
+      $('#div_alert').showAlert({message: "{{Vous êtes en mode édition vous pouvez redimensionner les widgets}}", level: 'info'});
+      $(this).attr('data-mode',1);
+      editWidgetMode(1);
+      $(this).css('color','rgb(46, 176, 75)');
+    }
+  });
+}, 1);
+
+
+ $('#bt_displayObject').on('click', function () {
+  if ($(this).attr('data-display') == 1) {
+    $('#div_displayObjectList').hide();
+    $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-12 col-md-12 col-sm-12');
+    $('.div_displayEquipement').each(function () {
+      $(this).packery();
+    });
+    $(this).attr('data-display', 0);
+  } else {
+    $('#div_displayObjectList').show();
+    $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-10 col-md-9 col-sm-8');
+    $('.div_displayEquipement').packery();
+    $(this).attr('data-display', 1);
+  }
+});
+
+ function editWidgetMode(_mode){
+  if(!isset(_mode)){
+    _mode = $('#bt_editDashboardWidgetOrder').attr('data-mode');
+    if(_mode == undefined){
+      return;
+    }
+  }
+  if(_mode == 0){
+   $('.div_displayEquipement .eqLogic-widget.allowResize').resizable('destroy');
+ }else{
+   $( ".div_displayEquipement .eqLogic-widget.allowResize").resizable({
+    grid: [ 40, 80 ],
+    resize: function( event, ui ) {
+     var el = ui.element;
+     el.closest('.div_displayEquipement').packery();
+   },
+   stop: function( event, ui ) {
+    var el = ui.element;
+    positionEqLogic(el.attr('data-eqlogic_id'));
+    el.closest('.div_displayEquipement').packery();
+    var eqLogic = {id : el.attr('data-eqlogic_id')}
+    eqLogic.display = {};
+    eqLogic.display.width =  Math.floor(el.width() / 40) * 40 + 'px';
+    eqLogic.display.height = Math.floor(el.height() / 80) * 80+ 'px';
+    jeedom.eqLogic.simpleSave({
+      eqLogic : eqLogic,
+      error: function (error) {
+        $('#div_alert').showAlert({message: error.message, level: 'danger'});
+      }
+    });
+  }
+});
+ }
 }
