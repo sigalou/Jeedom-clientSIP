1416f71ae39570778300c65a16dcffa55bbef3d8
Halbout Michael <michaelhalbout@gmail.com>
2017-08-31 12:11:50 +0200

    Create clientSIP.ajax.php

---
+++ core/ajax/clientSIP.ajax.php
@@ -0,0 +1,23 @@
+<?php
+try {
+	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+    	include_file('core', 'authentification', 'php');
+	include_file('core', 'dpt', 'class', 'eibd');
+    	if (!isConnect('admin')) {
+        	throw new Exception(__('401 - Accès non autorisé', __FILE__));
+    	}
+	if (init('action') == 'updateCmd') {
+    $cmd=cmd::byId(init('id'));
+    if(is_object($cmd)){
+      $cmd->event(init('value'));
+		  log::add('clientSIP','debug','Mise a jours du status: '.$value);
+		  ajax::success(true);
+    }
+		ajax::success(false);
+	}
+   throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+    /*     * *********Catch exeption*************** */
+} catch (Exception $e) {
+    ajax::error(displayExeption($e), $e->getCode());
+}
+?>
