6fa411ff2d593df7de5f317ade2d57eb3c7e45b1
Halbout Michael <michaelhalbout@gmail.com>
2017-08-31 12:12:31 +0200

    Update clientSIP.ajax.php

--- core/ajax/clientSIP.ajax.php
+++ core/ajax/clientSIP.ajax.php
@@ -7,16 +7,15 @@ try {
         	throw new Exception(__('401 - Accès non autorisé', __FILE__));
     	}
 	if (init('action') == 'updateCmd') {
-    $cmd=cmd::byId(init('id'));
-    if(is_object($cmd)){
-      $cmd->event(init('value'));
-		  log::add('clientSIP','debug','Mise a jours du status: '.$value);
-		  ajax::success(true);
-    }
+   		$cmd=cmd::byId(init('id'));
+    		if(is_object($cmd)){
+      			$cmd->event(init('value'));
+		  	log::add('clientSIP','debug','Mise a jours du status: '.$value);
+		  	ajax::success(true);
+		}
 		ajax::success(false);
 	}
    throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
-    /*     * *********Catch exeption*************** */
 } catch (Exception $e) {
     ajax::error(displayExeption($e), $e->getCode());
 }
