7c1465cb7561627eea6756af3e71fbc40cf81207
Halbout Michael <michaelhalbout@gmail.com>
2017-09-01 09:24:14 +0200

    Update clientSIP.php

--- desktop/php/clientSIP.php
+++ desktop/php/clientSIP.php
@@ -78,110 +78,130 @@ $eqLogics = eqLogic::byType('clientSIP');
 	</ul>
 		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-				<br>
-				<form class="form-horizontal">
-					<fieldset>			
-						<div class="form-group">
-							<label class="col-md-2 control-label">
-								{{Nom de l'équipement}}
-								<sup>
-									<i class="fa fa-question-circle tooltips" title="Indiquez le nom de votre équipement" style="font-size : 1em;color:grey;"></i>
-								</sup>
-							</label>
-							<div class="col-md-3">
-								<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-								<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
+				<div class="col-sm-6">
+					<form class="form-horizontal">
+						<legend>Général</legend>
+						<fieldset>		
+							<div class="form-group">
+								<label class="col-md-2 control-label">
+									{{Nom de l'équipement}}
+									<sup>
+										<i class="fa fa-question-circle tooltips" title="Indiquez le nom de votre équipement" style="font-size : 1em;color:grey;"></i>
+									</sup>
+								</label>
+								<div class="col-md-3">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
+								</div>
 							</div>
-						</div>
-						<div class="form-group expertModeVisible">
-							<label class="col-md-2 control-label ">{{Adresse }}
-								<sup>
-									<i class="fa fa-question-circle tooltips" title="Indiquez l'adresse de votre équipement. " style="font-size : 1em;color:grey;"></i>
-								</sup>
-							</label>
-							<div class="col-md-3">
-								<input type="text" class="eqLogicAttr form-control" data-l1key="logicalId"/>
+							<div class="form-group expertModeVisible">
+								<label class="col-md-2 control-label ">{{Adresse }}
+									<sup>
+										<i class="fa fa-question-circle tooltips" title="Indiquez l'adresse de votre équipement. " style="font-size : 1em;color:grey;"></i>
+									</sup>
+								</label>
+								<div class="col-md-3">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="logicalId"/>
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-md-2 control-label" >
-								{{Objet parent}}
-								<sup>
-									<i class="fa fa-question-circle tooltips" title="Indiquez l'objet dans lequel le widget de cette equipement apparaiterai sur le dashboard" style="font-size : 1em;color:grey;"></i>
-								</sup>
-							</label>
-							<div class="col-md-3">
-								<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+							<div class="form-group">
+								<label class="col-md-2 control-label" >
+									{{Objet parent}}
+									<sup>
+										<i class="fa fa-question-circle tooltips" title="Indiquez l'objet dans lequel le widget de cette equipement apparaiterai sur le dashboard" style="font-size : 1em;color:grey;"></i>
+									</sup>
+								</label>
+								<div class="col-md-3">
+									<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+										<?php
+										foreach (object::all() as $object) {
+											echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+										}
+										?>
+									</select>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-2 control-label">
+									{{Catégorie}}
+									<sup>
+										<i class="fa fa-question-circle tooltips" title="Choisissez une catégorie
+									Cette information n'est pas obigatoire mais peut etre utile pour filtrer les widget" style="font-size : 1em;color:grey;"></i>
+									</sup>
+								</label>
+								<div class="col-md-8">
 									<?php
-									foreach (object::all() as $object) {
-										echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+									foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+										echo '<label class="checkbox-inline">';
+										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+										echo '</label>';
 									}
 									?>
-								</select>
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-md-2 control-label">
-								{{Catégorie}}
-								<sup>
-									<i class="fa fa-question-circle tooltips" title="Choisissez une catégorie
-								Cette information n'est pas obigatoire mais peut etre utile pour filtrer les widget" style="font-size : 1em;color:grey;"></i>
-								</sup>
-							</label>
-							<div class="col-md-8">
-								<?php
-								foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-									echo '<label class="checkbox-inline">';
-									echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-									echo '</label>';
-								}
-								?>
 
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-sm-2 control-label" >
-								{{Etat du widget}}
-								<sup>
-									<i class="fa fa-question-circle tooltips" title="Choisissez les options de visibilité et d'activation
-								Si l'equipement n'est pas activé il ne sera pas utilisable dans jeedom, mais visible sur le dashboard
-								Si l'equipement n'est pas visible il ne sera caché sur le dashbord, mais utilisable dans jeedom" style="font-size : 1em;color:grey;"></i>
-								</sup>
-							</label>
-							<div class="col-sm-9">
-								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+							<div class="form-group">
+								<label class="col-sm-2 control-label" >
+									{{Etat du widget}}
+									<sup>
+										<i class="fa fa-question-circle tooltips" title="Choisissez les options de visibilité et d'activation
+									Si l'equipement n'est pas activé il ne sera pas utilisable dans jeedom, mais visible sur le dashboard
+									Si l'equipement n'est pas visible il ne sera caché sur le dashbord, mais utilisable dans jeedom" style="font-size : 1em;color:grey;"></i>
+									</sup>
+								</label>
+								<div class="col-sm-9">
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-md-2 control-label" >
-								{{Autentification}}
-								<sup>
-									<i class="fa fa-question-circle tooltips" title="" style="font-size : 1em;color:grey;"></i>
-								</sup>
-							</label>
-							<div class="col-md-3">
-								<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="Username" placeholder="{{Login}}"/>
-								<input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="Password" placeholder="{{Mots de passe}}"/>
+						</fieldset>
+					</form>
+				</div>			
+				<div class="col-sm-6">
+					<form class="form-horizontal">
+						<legend>Compte Sip</legend>
+						<fieldset>
+							<div class="form-group">
+								<label class="col-md-2 control-label" >
+									{{Autentification}}
+									<sup>
+										<i class="fa fa-question-circle tooltips" title="" style="font-size : 1em;color:grey;"></i>
+									</sup>
+								</label>
+								<div class="col-md-3">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="Username" placeholder="{{Login}}"/>
+									<input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="Password" placeholder="{{Mots de passe}}"/>
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-md-2 control-label" >
-								{{Transport}}
-								<sup>
-									<i class="fa fa-question-circle tooltips" title="Choisissez le type de transport" style="font-size : 1em;color:grey;"></i>
-								</sup>
-							</label>
-							<div class="col-md-3">
-								<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="transport">
-									<option value="udp">UDP</option>
-									<option value="tcp">TCP</option>
-									<option value="tls">TLS</option>
-								</select>
+							<div class="form-group">
+								<label class="col-md-2 control-label" >
+									{{Expiration}}
+									<sup>
+										<i class="fa fa-question-circle tooltips" title="Saisire le delais d'expiration" style="font-size : 1em;color:grey;"></i>
+									</sup>
+								</label>
+								<div class="col-md-3">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="Expiration" placeholder="{{Expiration}}"/>
+								</div>
 							</div>
-						</div>
-					</fieldset> 
-				</form>
+							<div class="form-group">
+								<label class="col-md-2 control-label" >
+									{{Transport}}
+									<sup>
+										<i class="fa fa-question-circle tooltips" title="Choisissez le type de transport" style="font-size : 1em;color:grey;"></i>
+									</sup>
+								</label>
+								<div class="col-md-3">
+									<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="transport">
+										<option value="udp">UDP</option>
+										<option value="tcp">TCP</option>
+										<option value="tls">TLS</option>
+									</select>
+								</div>
+							</div>
+						</fieldset> 
+					</form>
+				</div>
 			</div>
 			<div role="tabpanel" class="tab-pane" id="commandtab">
 				<br>
