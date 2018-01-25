893b418f62b09590c774fe916948f3c4010d1882
Halbout Michael <michaelhalbout@gmail.com>
2018-01-25 09:07:40 +0100

    Update clientSIP.Player.php

--- desktop/modal/clientSIP.Player.php
+++ desktop/modal/clientSIP.Player.php
@@ -1,7 +1,7 @@
 <OBJECT classid="clsid:9BE31822-FDAD-461B-AD51-BE1D1C159921"
      codebase="http://downloads.videolan.org/pub/videolan/vlc/latest/win32/axvlc.cab"
      width="640" height="480" id="vlc" events="True">
-   <param name="Src" value="<?php echo $_REQUEST['rtp']; ?P" />
+   <param name="Src" value="<?php echo $_REQUEST['rtp']; ?>" />
    <param name="ShowDisplay" value="True" />
    <param name="AutoLoop" value="False" />
    <param name="AutoPlay" value="True" />
